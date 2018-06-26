e47e6bd5e9c8677c24eac3ae7b1c2619cecf85e9
Дмитрий Ромашов <framebassman@gmail.com>
2018-06-26 23:49:14 +0300

    Add tag number

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -1,3 +1,5 @@
-docker build --file ./Dockerfile .
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push framebassman/fake-sender
+if [[ $TRAVIS_BUILD_NUMBER != "" ]]; then
+  docker build --file ./Dockerfile . --tag framebassman/fake-sender:1.0.$TRAVIS_BUILD_NUMBER
+  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+  docker push framebassman/fake-sender
+fi
