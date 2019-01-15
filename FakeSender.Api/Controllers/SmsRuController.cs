c33046893dee6a2a32dcf77e1820d4264fa1d3df
Dmitry Romashov <framebassman@gmail.com>
2019-01-15 14:15:08 +0300

    fix error in controller constructor

--- FakeSender.Api/Controllers/SmsRuController.cs
+++ FakeSender.Api/Controllers/SmsRuController.cs
@@ -1,6 +1,8 @@
+using System;
 using FakeSender.Api.Data;
 using FakeSender.Api.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite.Internal.UrlMatches;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
@@ -12,17 +14,24 @@ namespace FakeSender.Api.Controllers
         private readonly ApplicationContext _db;
         private readonly ILogger _logger;
         
-        public SmsRuController(ApplicationContext context, DbSet<Sms> box, ILogger<StorageController<Sms>> logger)
-            : base(context, box, logger)
+        public SmsRuController(ApplicationContext context, ILogger<SmsRuController> logger) 
+            : base(context, context.SmsBox, logger)
         {
             _db = context;
             _logger = logger;
         }
 
         [HttpGet("send")]
-        public IActionResult Send()
+        public IActionResult Send(
+            [FromQuery] Guid apiKey,
+            [FromQuery] String from,
+            [FromQuery] Int32 json,
+            [FromQuery] String msg,
+            [FromQuery] String to
+        )
         {
-            return null;
+            _logger.LogInformation($"Received message to {to}"); 
+            return new OkResult();
         }
     }
 }
