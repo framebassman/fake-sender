feb4c776b4b5cfd3fd7633a57b23496cd02ddb57
Dmitry Romashov <framebassman@gmail.com>
2019-01-16 12:30:17 +0300

    add tests for SmsRuController

---
+++ FakeSender.Api.Tests/ControllersTests/SmsRuControllerTests/SmsRuTestsFixture.cs
@@ -0,0 +1,43 @@
+using System;
+using FakeSender.Api.Controllers;
+using FakeSender.Api.Data;
+using FakeSender.Api.Models;
+
+namespace FakeSender.Api.Tests.ControllersTests.SmsRuControllerTests
+{
+    public abstract class SmsRuTestsFixture : ControllerTestsBase<SmsRuController>, IDisposable
+    {
+        protected readonly Account WithPositiveBalance;
+        protected readonly Account WithNegativeBalance;
+        protected readonly ApplicationContext Db;
+        
+        protected SmsRuTestsFixture()
+        {
+            this.Db = new ApplicationContext(Options);
+            this.WithPositiveBalance = new Account
+            {
+                Service = "smsru",
+                Type = "sms",
+                Balance = 100,
+                Login = "positive"
+            };
+            this.WithNegativeBalance = new Account
+            {
+                Service = "smsru",
+                Type = "sms",
+                Balance = 0,
+                Login = "negative"
+            };
+            this.Db.Accounts.Add(this.WithPositiveBalance);
+            this.Db.Accounts.Add(this.WithNegativeBalance);
+            this.Db.SaveChanges();
+        }
+        
+        public void Dispose()
+        {
+            this.Db.Accounts.RemoveRange();
+            this.Db.SaveChanges();
+            this.Db.Dispose();
+        }
+    }
+}
---
+++ FakeSender.Api.Tests/ControllersTests/SmsRuControllerTests/SmsShouldBeSaved.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Linq;
+using FakeSender.Api.Controllers;
+using FakeSender.Api.Controllers.Responses;
+using FakeSender.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace FakeSender.Api.Tests.ControllersTests.SmsRuControllerTests
+{
+    public class SmsShouldBeSaved : SmsRuTestsFixture
+    {
+        private readonly SmsRuController _controller;
+
+        public SmsShouldBeSaved()
+        {
+            this._controller = new SmsRuController(this.Db, this.Logger);
+        }
+
+        [Fact]
+        public void PhoneIsMobile_And_BalanceIsPositive_Then_SmsShouldBeSaved()
+        {
+            // Arrange 
+            var phone = new Phone(7, 999, 9999999);
+            var message = "Hi";
+            var expected = new Sms
+            {
+                To = phone.ToString(),
+                Message = message
+            };
+            
+            // Act
+            var result = this._controller.Send(
+                Guid.Empty,
+                this.WithPositiveBalance.Login, 
+                1,
+                Uri.EscapeUriString(message),
+                phone.ToString()
+            );
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(expected.ToJson(), this.Db.SmsBox.First(s => s.To == phone.ToString()).ToJson());
+        }
+        
+        [Fact]
+        public void PhoneIsNotMobile_And_BalanceIsPositive_Then_SmsShouldNotBeSaved()
+        {
+            // Arrange 
+            var phone = new Phone(7, 812, 9999999);
+            var message = "Hi";
+            
+            // Act
+            var result = this._controller.Send(
+                Guid.Empty,
+                this.WithPositiveBalance.Login, 
+                1,
+                Uri.EscapeUriString(message),
+                phone.ToString()
+            );
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+            Assert.False(
+                this.Db.SmsBox.Any(s => s.To == phone.ToString()),
+                "Sms message should not be saved in SmsBox"
+            );
+        }
+        
+        [Theory]
+        [InlineData("79999999999")]
+        [InlineData("78129999999")]
+        public void BalanceIsNegative_Then_SmsShouldNotBeSaved(string phoneSource)
+        {
+            // Arrange 
+            var phone = new Phone(phoneSource);
+            var message = "Hi";
+            
+            // Act
+            var result = this._controller.Send(
+                Guid.Empty,
+                this.WithNegativeBalance.Login, 
+                1,
+                Uri.EscapeUriString(message),
+                phone.ToString()
+            );
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+            Assert.False(
+                this.Db.SmsBox.Any(s => s.To == phone.ToString()),
+                "Sms message should not be saved in SmsBox"
+            );
+        }
+    }
+}
