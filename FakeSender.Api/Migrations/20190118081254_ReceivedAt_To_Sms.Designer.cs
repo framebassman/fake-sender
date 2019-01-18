64d9c5026d60483e2aa72774cad6ed1cd31ab34b
Dmitry Romashov <framebassman@gmail.com>
2019-01-18 11:15:04 +0300

    add ReceivedAt to sms

--- FakeSender.Api/Migrations/20190118081254_ReceivedAt_To_Sms.Designer.cs
+++ FakeSender.Api/Migrations/20190118081254_ReceivedAt_To_Sms.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using FakeSender.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FakeSender.Api.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20190118081254_ReceivedAt_To_Sms")]
+    partial class ReceivedAt_To_Sms
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- FakeSender.Api/Models/Sms.cs
+++ FakeSender.Api/Models/Sms.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
 
 namespace FakeSender.Api.Models
 {
@@ -6,6 +8,8 @@ namespace FakeSender.Api.Models
     {
         public string To { get; set; }
         public string Message { get; set; }
+        [JsonIgnore]
+        public DateTime ReceivedAt { get; set; }
         
         [NotMapped]
         public override string EntityId => To;
